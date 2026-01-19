1cdb8864a8d5155c529102fb2b1f3acd35ec844a
yangzheng <1345509173@q.com>
2026-01-19 18:29:32 +0800

    update m1v5 keyboard

--- keyboards/monsgeek/m1_v5/m1_v5_uk/m1_v5_uk.c
+++ keyboards/monsgeek/m1_v5/m1_v5_uk/m1_v5_uk.c
@@ -66,7 +66,7 @@ static uint32_t ee_clr_timer = 0;
 static uint32_t rec_time;
 bool test_white_light_flag = false;
 HSV start_hsv;
-bool no_record_fg;
+bool no_record_fg,im_test_rate_flag;
 bool lower_sleep = false;
 uint8_t buff[]   = {14, 8, 2, 1, 1, 1, 1, 1, 1, 1, 0};
 
